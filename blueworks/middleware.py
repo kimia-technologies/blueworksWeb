c2e654bd786c079b2ade8343d5d58fa236b22f48
kevin <ulrichkev2017@gmail.com>
2019-10-11 19:10:35 +0200

    v1.1.2

--- blueworks/middleware.py
+++ blueworks/middleware.py
@@ -1,5 +1,9 @@
-
 import re
+
+from django.conf import settings
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib.auth import logout
 
 
 class RbacMiddleware():
@@ -7,25 +11,27 @@ class RbacMiddleware():
         self.get_response = get_response
 
     def __call__(self, request):
-        response = None
-        regex_http_ = re.compile(r'^HTTP_.+$')
-        regex_content_type = re.compile(r'^CONTENT_TYPE$')
-        regex_content_length = re.compile(r'^CONTENT_LENGTH$')
-
-        self.request_headers = {}
-        for header in request.META:
-            if regex_http_.match(header) or regex_content_type.match(header) or regex_content_length.match(header):
-                self.request_headers[header] = request.META[header]
-        # if hasattr(self, 'process_request'):
-        #     print('request')
-        #     response = self.process_request(request)
-        # if not response:
-        #     print(self.request_headers)
-        #     response = self.get_response(request)
-        # if hasattr(self, 'process_response'):
-        #     print('response')
-        #     response = self.process_response(request, response)
-        print('avant')
         response = self.get_response(request)
-        print('apres')
-        return self.get_response(request)
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        return redirect('sites.html')
+
+        # if path == reverse('accounts:logout').lstrip('/'):
+        #     logout(request)
+
+        # if request.user.is_authenticated() and url_is_exempt:
+        #     return redirect(settings.LOGIN_REDIRECT_URL)
+        # elif request.user.is_authenticated() or url_is_exempt:
+        #     return None
+        # else:
+        #     return redirect(settings.LOGIN_URL)
+
+        # regex_http_ = re.compile(r'^HTTP_.+$')
+        # regex_content_type = re.compile(r'^CONTENT_TYPE$')
+        # regex_content_length = re.compile(r'^CONTENT_LENGTH$')
+
+        # self.request_headers = {}
+        # for header in request.META:
+        #     if regex_http_.match(header) or regex_content_type.match(header) or regex_content_length.match(header):
+        #         self.request_headers[header] = request.META[header]
--- blueworksManageApp/settings.py
+++ blueworksManageApp/settings.py
@@ -47,12 +47,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # 'django.middleware.security.SecurityMiddleware',
-    'blueworks.middleware.RbacMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    # 'blueworks.middleware.RbacMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
